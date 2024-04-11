944878a8c34303ac5e1fb22c5ee1c2f56592d01a
brunogoniadis <brunogoniadislima@gmail.com>
2024-04-11 00:36:19 -0300

    feat: Created initial files in react  typescript

---
+++ movie-biox-reactTs-Frontend/src/main.tsx
@@ -0,0 +1,33 @@
+
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import GlobalStyle from './styles/GlobalStyle.ts'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import { Movie } from './pages/Movie/Movie.tsx'
+
+import { Home } from './pages/Home/Home.tsx'
+
+
+import { Search } from './pages/Search/Search.tsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+
+  <>
+    <GlobalStyle />
+
+    <Router>
+      <Routes>
+        <Route element={<App />}>
+
+          <Route path="/" element={<Home />} />
+          <Route path="movie/:id" element={<Movie />} />
+          <Route path="search" element={<Search />} />
+        </Route>
+      </Routes>
+
+    </Router>
+
+  </>
+)
