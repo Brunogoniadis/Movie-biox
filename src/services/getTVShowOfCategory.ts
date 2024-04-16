b6819286d58af669aa10f7d4d9e58d00251ba5b3
brunogoniadis <brunogoniadislima@gmail.com>
2024-04-16 03:13:57 -0300

    update: link TvShowOfCategory in vercel

--- src/services/getTVShowOfCategory.ts
+++ src/services/getTVShowOfCategory.ts
@@ -7,12 +7,12 @@ export const getTVShowsByCategory = async (
 ): Promise<ITvShow[]> => {
   const apiKey = import.meta.env.VITE_API_KEY;
 
-  const apiUrl: string = `https://api.themoviedb.org/3/discover/tv?api_key=${apiKey}&with_genres=${genreId}&page=${page}`;
+  const apiUrl: string = `https://node-ts-moviebiox.vercel.app/api/tv/category/${genreId}?page=${page}`;
 
   try {
     const response = await axios.get(apiUrl);
 
-    return response.data.results;
+    return response.data;
   } catch (error) {
     console.error('Error:', error);
     return [];
