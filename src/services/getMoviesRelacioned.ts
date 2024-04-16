372f96f5cc76b8f4932d56c98c34cde8be240a6c
brunogoniadis <brunogoniadislima@gmail.com>
2024-04-16 03:15:28 -0300

    update: link getMoviewRelacioned in vercel

--- src/services/getMoviesRelacioned.ts
+++ src/services/getMoviesRelacioned.ts
@@ -5,13 +5,11 @@ import { IMovie } from './types/IMovie';
 export const getMoviesRelacioned = async (
   movieId: string | undefined
 ): Promise<IMovie[]> => {
-  const apiKey = import.meta.env.VITE_API_KEY;
-
-  const apiUrl: string = `https://api.themoviedb.org/3/movie/${movieId}/recommendations?api_key=${apiKey}`;
+  const apiUrl: string = `https://node-ts-moviebiox.vercel.app/api/movies/related/${movieId}}`;
 
   try {
     const response = await axios.get(apiUrl);
-    return response.data.results;
+    return response.data;
   } catch (error) {
     console.error('Error:', error);
     return [];
